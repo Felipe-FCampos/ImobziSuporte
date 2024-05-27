27ad2f4b8e1c989b1fe2b7c5fc82f7158734da82
Felipe Campos <felipe.fcampos7@gmail.com>
2024-05-27 16:51:42 -0300

    v1.13.2

--- src/app/calcula-imposto/calcula-imposto.component.ts
+++ src/app/calcula-imposto/calcula-imposto.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewContainerRef } from '@angular/core';
 import { SidebarService } from '../sidebar.service';
 import { HistoryService } from '../history.service';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-calcula-imposto',
@@ -10,10 +11,19 @@ import { HistoryService } from '../history.service';
 
 export class CalculaImpostoComponent {
 
+  valueAdmFee!: number;
+  opcaoAdmFee!: string;
+  opcao!: string;
+  add!: string;
+  rent!: number;
+  recipient!: number;
+  resultado!: number;
+
   constructor(
     private sidebarService: SidebarService, 
     private viewContainerRef: ViewContainerRef, 
-    private historyService: HistoryService
+    private historyService: HistoryService,
+    private apiService: ApiService
   ) { }
 
   // Verifica a opcao recebida e habilita o campo de taxa de adm
@@ -26,7 +36,67 @@ export class CalculaImpostoComponent {
   }
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+  // Outros métodos --------------->
 
+  openSideBar() {
+    this.sidebarService.toggleSideBar(this.viewContainerRef);
+  }
+
+  // Guarda informação no localStorage
+  saveCalculatorValue(valorFinal: number) {
+    this.historyService.addHistory(valorFinal);
+  }
+
+  getLastNumber(lastNumber: number){
+    this.historyService.saveLastNumber(lastNumber);
+  }
+
+  saveSimpleDiscount(opcao: string){
+    this.historyService.addHistorySimpleDicount(opcao);
+  }
+
+  saveAdmFeeDeduct(opcao: string){
+    this.historyService.addHistoryAdmFee(opcao);
+  }
+
+  saveAdmFee(value: number){
+    this.historyService.addAdmFee(value);
+  }
+
+
+  // Nova versão integrada à API
+
+  somar(){
+
+    let inputRent = document.getElementById("rent") as HTMLInputElement;
+    let inputRecipient = document.getElementById("recipient") as HTMLInputElement;
+    let inputAdmFee = document.getElementById("admFee") as HTMLInputElement;
+
+    let opcaoSimpleDiscount = document.querySelector('input[name="optionSimplifiedDiscount"]:checked') as HTMLInputElement;
+    let opcaoAdmFee = document.querySelector('input[name="optionAdmFee"]:checked') as HTMLInputElement;
+
+    this.rent = parseFloat(inputRent.value);
+    this.recipient = parseFloat(inputRecipient.value);
+    this.opcao = opcaoSimpleDiscount.value;
+    this.opcaoAdmFee = opcaoAdmFee.value;
+    this.valueAdmFee = parseFloat(inputAdmFee.value);
+    
+    this.apiService.sendItems(this.rent, this.recipient, this.opcao, this.opcaoAdmFee, this.valueAdmFee).subscribe(items => {
+      this.resultado = items.resultado
+      this.opcao = this.add
+      this.opcaoAdmFee = this.add
+      console.log(this.resultado)
+    })
+    console.log(this.resultado)
+  }
+}
+
+
+
+
+
+// Versão anterior do cálculo
+/*
   // Verifica qual a opcao selecionada na taxa de adm
   selectAdmFee() {
     const selectedOption = document.querySelector('input[name="optionAdmFee"]:checked') as HTMLInputElement;
@@ -182,31 +252,4 @@ export class CalculaImpostoComponent {
   }
 
 
-
-  // Outros métodos --------------->
-
-  openSideBar() {
-    this.sidebarService.toggleSideBar(this.viewContainerRef);
-  }
-
-  // Guarda informação no localStorage
-  saveCalculatorValue(valorFinal: number) {
-    this.historyService.addHistory(valorFinal);
-  }
-
-  getLastNumber(lastNumber: number){
-    this.historyService.saveLastNumber(lastNumber);
-  }
-
-  saveSimpleDiscount(opcao: string){
-    this.historyService.addHistorySimpleDicount(opcao);
-  }
-
-  saveAdmFeeDeduct(opcao: string){
-    this.historyService.addHistoryAdmFee(opcao);
-  }
-
-  saveAdmFee(value: number){
-    this.historyService.addAdmFee(value);
-  }
-}
+*/
